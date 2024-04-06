9c77293e2ff39d5173c2ea310832d841293c2df8
swishyy <famodavid1@gmail.com>
2024-04-06 22:20:37 +0100

    Added system tray icon

--- main.py
+++ main.py
@@ -1,17 +1,27 @@
-# Useful link
-# https://stackoverflow.com/questions/65311471/returning-an-argument-on-key-press-detection-using-pynput
-
 from pynput.keyboard import Listener
+from threading import Event
+from infi.systray import SysTrayIcon
 from bindHandler import handleBinds
 from win32gui import GetForegroundWindow, GetWindowText
+import webbrowser as web
 
 paintFocused = False
 listening = True
 listener = Listener(on_press=lambda event: on_press(event))
 listener.start()
 
+quit_event = Event()
+
 print("MSPaint keybinds running...")
 
+def openRepo(systray):
+    web.open("https://github.com/kakajuro/mspaintbinds")
+
+def qcallback(systray):
+    global listening
+    quit_event.set()
+    listening = False
+    
 def on_press(key): 
     global listening
     if 'char' in dir(key) and paintFocused:
@@ -20,7 +30,12 @@ def on_press(key):
         if res == False:
           listening = False
 
-while listening:
+menu_options = (("Source Code Repository", None, openRepo, ),)
+systray = SysTrayIcon("imgs/paintlogo.ico", "MSPaint Binds", menu_options, on_quit=qcallback)
+
+with systray:
+    
+  while listening:
     
     try:
       windowTitle = GetWindowText(GetForegroundWindow())
@@ -33,5 +48,6 @@ while listening:
         
     except Exception as e:
       print("Something went wrong.")
-    
+      
 listener.stop()
+print("Closing program...")
